2631f609b85f9690c861092df3fbf364bbf38770
unknown <iiigogor@gmail.com>
2022-01-10 21:04:03 +0300

    inided

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,36 @@
+package ru.netology.domain;
+
+public class LikesInfo {
+    private int id;
+    private int ownerId;
+    private int countId;
+    private int data;
+    private String typeLike;
+        // + get/set на все поля
+   return int countLikes;
+    }
+
+public class CommentsInfo {
+    private int authorid;
+    private int countId;
+    private int data;
+    private String toWhomComments;
+    private String textInfo;
+    return int countComments;
+     // + get/set на все поля
+}
+public class RepostsInfo  {
+    private int ownerPostId;
+    private boolean userRepost;
+
+    // + get/set на все поля
+    return int countRepost;
+}
+public class countView {
+    private int countID;
+    // + get/set
+    return int countViews;
+
+}
+
+
